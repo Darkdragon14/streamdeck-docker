122f1989d6c587c85819b2b9141520e967b68a10
Darkdragon14 <s.tristan@hotmail.fr>
2024-12-09 21:34:09 +0100

    chore: add JSDoc

--- src/utils/pingDocker.ts
+++ src/utils/pingDocker.ts
@@ -1,5 +1,14 @@
 import { Docker } from "node-docker-api";
 
+/**
+ * Pings the Docker daemon to check if it is reachable.
+ *
+ * @param {Docker} docker - The Docker instance used to send the ping request.
+ * @param {any} ev - The event object containing the action to update the state in case of failure.
+ * @param {number} state - The state to set if the ping fails.
+ * @returns {Promise<boolean>} - Resolves to `true` if the ping succeeds, `false` otherwise.
+ * @throws {Error} - Propagates any unexpected errors during the ping process.
+ */
 export async function pingDocker(docker: Docker, ev: any, state: number): Promise<boolean> {
 	try {
 		await docker.ping();
